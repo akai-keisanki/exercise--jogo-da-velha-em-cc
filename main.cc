bb51c4b8db40422d8a256322d9ea8672c5e7ed60
akai-keisanki <iago.ad@outlook.com>
2025-04-24 20:16:44 -0300

    feat(UI): added table printing

--- main.cc
+++ main.cc
@@ -9,25 +9,25 @@ protected:
   std::bitset<9> x;
   std::bitset<9> o;
 
-  char get_game_cell (const size_t& i, const size_t& j)
+  char get_table_cell (const size_t& i, const size_t& j)
   {
     if (x.test(i * 3 + j)) return 'X';
     if (o.test(i * 3 + j)) return 'O';
     return '0' + (i * 3 + j + 1);
   }
 
-  void print_game_row (const size_t& i)
+  void print_table_row (const size_t& i)
   {
-    std::cout << ' ' << get_game_cell(i, 0) << " | " << get_game_cell(i, 1) << " | " << get_game_cell(i, 2) << ' ' << std::endl;
+    std::cout << ' ' << get_table_cell(i, 0) << " | " << get_table_cell(i, 1) << " | " << get_table_cell(i, 2) << ' ' << std::endl;
   }
 
-  void print_game (void)
+  void print_table (void)
   {
-    print_game_row(0);
+    print_table_row(0);
     std::cout << "-----------" << std::endl;
-    print_game_row(1);
+    print_table_row(1);
     std::cout << "-----------" << std::endl;
-    print_game_row(2);
+    print_table_row(2);
   }
 
 public:
