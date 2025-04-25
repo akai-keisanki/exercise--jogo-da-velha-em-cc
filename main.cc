c6766764aa450effd55ce532bda3b59b7a2ebd6e
akai-keisanki <iago.ad@outlook.com>
2025-04-24 21:25:36 -0300

    feat(Logic & UI): added logical functions for the game and basic UI.

--- main.cc
+++ main.cc
@@ -30,14 +30,115 @@ protected:
     print_table_row(2);
   }
 
+  bool occupy_pos_w_x (const size_t& i)
+  {
+    if (i > 9 || o.test(i - 1)) return false;
+    x.set(i - 1);
+    return true;
+  }
+
+  bool occupy_pos_w_o (const size_t& i)
+  {
+    if (i > 9 || x.test(i - 1)) return false;
+    o.set(i - 1);
+    return true;
+  }
+
+  bool test_result_for (const std::bitset<9>& b)
+  {
+    for (size_t i = 0; i < 3; i ++)
+    {
+      if (x.test(i)     && x.test(3 + i)     && x.test(6 + i)
+      ||  x.test(3 * i) && x.test(3 * i + 1) && x.test(3 * i + 2)) return true;
+    }
+
+    if (x.test(4) && (x.test(0) && x.test(8) || x.test(6) && x.test(2))) return true;
+
+    return false;
+  }
+
+  bool test_draw (void)
+  {
+    for (size_t i = 0; i < 9; i ++)
+      if (!(x.test(i) || o.test(i))) return false;
+
+    return true;
+  }
+
+  short test_result (void)
+  {
+    return test_result_for(x) ? 1 : test_result_for(o) ? 2 : test_draw() ? -1 : 0;
+  }
+
+  bool ui_game_end (void)
+  {
+    short r = test_result();
+
+    switch (r)
+    {
+    case 1:
+    case 2:
+      std::cout << "Parabéns! Jogador " << r << " venceu!" << std::endl;
+      return true;
+
+    case 0:
+      return false;
+
+    case -1:
+      std::cout << "Empate! Nenhum jogador venceu." << std::endl;
+      return true;
+
+    }
+    return false;
+  }
+
+  void restart_prompt (void)
+  {
+    char c;
+
+    std::cout << "Deseja reiniciar o jogo? (S/N): ";
+    std::cin >> c;
+
+    while (true)
+    {
+      switch (c)
+      {
+      case 'S':
+      case 's':
+        run_program_user_interface();
+        return;
+
+      case 'N':
+      case 'n':
+        return;
+
+      default:
+        std::cout << "Opção inválida. Deseja reiniciar o jogo? (S/N): ";
+      }
+    }
+  }
+
 public:
 
   TicTacToeProgram ()
   {}
 
+  void run_program_user_interface ()
+  {
+    do
+    {
+      if (ui_game_end()) break;
+    }
+    while (!ui_game_end());
+
+    restart_prompt();
+  }
+
 };
 
 signed main (void)
 {
+  TicTacToeProgram().run_program_user_interface();
+
   return 0;
 }
